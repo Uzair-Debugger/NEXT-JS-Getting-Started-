10da80ce141ad09eff858a7067bd0635d61db927
Uzair Debugger <your_email@example.com>
2025-11-09 23:59:51 +0500

    Simple Hello from backend route route

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,14 +8,13 @@ is optional. The compiler automatically handles JSX transformation, so including
 */
 
 import Hello from "../../components/Hello"
-import Navbar from "@/components/Navbar"
 const Home = () => {
   // we talked that NEXT by default creates server components so at this case the console.log should not appear in my browser console tab, but it appears why? because the new version of NEXT just sharing this for convenience
   // console.log('What type of a component am I?')
 
   return (
     <main>
-      <Navbar/>
+     
     <div className="text-5xl underline">Welcome to Next.Js Tutorial</div>
     <Hello /> 
     {/* This <Hello/> component will run in terminal as well altough it's a client component but because it's renderend in the server component as well so server will run it too after it's "hydration". */}
